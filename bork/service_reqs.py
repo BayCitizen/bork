cec809b8073920dc0cd28beea672c7915b117090
Joshua Bonnett <joshua.bonnett@gmail.com>
2012-08-08 17:42:52 -0700

    service reqs should now be able to make start on boot

--- bork/service_reqs.py
+++ bork/service_reqs.py
@@ -6,8 +6,9 @@ from . import Requirement
 class Service(Requirement):
     service_name = str()
     """Service is a wrapper for upstarts service protocol"""
-    def __init__(self, service_name=None, *arg, **kwargs):
+    def __init__(self, service_name=None, restart_on_boot=None, *arg, **kwargs):
         self.service_name = service_name
+        self.restart_on_boot
         super(Service, self).__init__()
 
     def start_service(self):
@@ -22,18 +23,42 @@ class Service(Requirement):
         return False
 
     def start_on_boot(self):
-        result = os.popen('/sbin/initctl show-config %s').read()
-        if 'start on runlevel [2345]' in result:
-            return True
+        result = os.popen('/sbin/initctl show-config %s' % self.service_name).read()
+        if self.service_name in result:
+            if 'start on runlevel [2345]' in result:
+                return True
+        else:
+            result = os.popen('ls /etc/rc*.d/S**%s' % self.service_name)
+            if self.service_name in result:
+                return True
         return False
 
     def make_start_on_boot(self):
-        raise NotImplementedError('havent made start on boot work for upstart')
+        if self.use_upstart:
+            f = open('/etc/init/%s.conf'%self.service_name, 'r+')
+            f.write("""#Start when system enters runlevel 2 (multi-user mode).
+start on runlevel [2345]
+start on runlevel [!2345]
+
+# Start delayed_job via the daemon control script.
+exec /etc/init.d/%s start
+
+# Restart the process if it dies with a signal
+# or exit code not given by the 'normal exit' stanza.
+respawn
+
+# Give up if restart occurs 10 times in 90 seconds.
+respawn limit 5 90
+""" % self.service_name)
+            f.close()
+        else:
+            os.popen("update-rc.d %s defaults" % self.service_name)
+
 
     def execute(self):
         super(Service, self).execute()
         if not self.is_running():
             self.start_service()
-        if not self.start_on_boot():
+        if not self.start_on_boot() and self.restart_on_boot:
             self.make_start_on_boot()
 
